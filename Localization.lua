87c64ee1b14b90d9092bcc66e2763dbec2ef130a
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-11-24 16:59:02 -0800

    release

---
+++ Localization.lua
@@ -0,0 +1,138 @@
+local SlowFaller, L = ...; -- Let's use the private table passed to every .lua 
+
+local function defaultFunc(L, key)
+ -- If this function was called, we have no localization for this key.
+ -- We could complain loudly to allow localizers to see the error of their ways, 
+ -- but, for now, just return the key as its own localization. This allows you to—avoid writing the default localization out explicitly.
+ return key;
+end
+setmetatable(L, {__index=defaultFunc});
+
+local LOCALE = GetLocale()
+
+local SpellNames = {
+
+Flightform = "\124cffffd000\124Hspell:276029\124h[" .. Spell:CreateFromSpellID(276029):GetSpellName() .. "]\124h\124r",
+Flap = "\124cffffd000\124Hspell:164862\124h[" .. Spell:CreateFromSpellID(164862):GetSpellName() .. "]\124h\124r",
+Slowfall = "\124cffffd000\124Hspell:130\124h[" .. Spell:CreateFromSpellID(130):GetSpellName() .. "]\124h\124r",
+Levitate = "\124cffffd000\124Hspell:1706\124h[" .. Spell:CreateFromSpellID(1706):GetSpellName() .. "]\124h\124r",
+
+};
+
+SpellNames.class = select(2, UnitClassBase("player"))
+
+if SpellNames.class == 5 then -- priest
+	SpellNames.spells = SpellNames.Levitate
+elseif SpellNames.class == 8 then -- mage
+	SpellNames.spells = SpellNames.Levitate
+--[[
+elseif SpellNames.class == 10 then -- monk
+	SpellNames.spells = SpellNames.Levitate
+]]
+elseif SpellNames.class == 11 then -- druid
+	SpellNames.spells = SpellNames.Flap --SpellNames.Flightform .. ", " .. 
+else
+	SpellNames.spells = SpellNames.Flap .. ", " .. SpellNames.Slowfall .. ", " .. SpellNames.Levitate --SpellNames.Flightform .. ", " .. 
+end
+
+if LOCALE == "enUS" then
+	-- The EU English game client also
+	-- uses the US English locale code.
+	L["Hello!"] = "Hello friend! The SlowFaller addon is now enabled on a class with a slow fall ability.\nDouble-jump to activate: " .. SpellNames.spells .. "\nDoes not work in combat or while mounted."
+	L["Oh no!"] = "Hello friend! This message is here to let you know that the SlowFaller addon is enabled on a class without a slow fall ability, and therefore will not be running any code.\nRequires: " .. SpellNames.spells
+	L["Name"] = "SlowFaller"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "esES" or LOCALE == "esMX" then -- official translation (thanks to twitter @RomanValoppi )
+	-- Spanish translations go here
+	L["Hello!"] = "¡Hola amigo! El complemento SlowFaller ahora está habilitado en una clase con una habilidad de caída lenta. Salta dos veces para activar: " .. SpellNames.spells .. "\nNo funciona en combate ni con montura."
+	L["Oh no!"] = "¡Hola amigo! Este mensaje está aquí para informarte que el complemento SlowFaller está habilitado en una clase sin una habilidad de caída lenta, por lo tanto, no ejecutará ningún código.\nRequiere: " .. SpellNames.spells
+	L["Name"] = "SlowFaller"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "deDE" then
+	-- German translations go here
+	L["Hello!"] = "Hallo Freund! Das SlowFaller-Addon ist jetzt für eine Klasse mit der Fähigkeit „Slow Fall“ aktiviert. Führt einen Doppelsprung aus: " .. SpellNames.spells .. "\nFunktioniert nicht im Kampf oder auf einem Pferd."
+	L["Oh no!"] = "Hallo Freund! Diese Nachricht soll Sie darüber informieren, dass das SlowFaller-Addon für eine Klasse ohne Slow-Fall-Fähigkeit aktiviert ist und daher keinen Code ausführt.\nBenötigt: "  .. SpellNames.spells
+	L["Name"] = "SlowFaller"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "frFR" then -- official translation (thanks to twitter @Solanya_ )
+	-- French translations go here
+	L["Hello!"] = "Salut l'ami! L'addon SlowFaller est maintenant activé sur une classe avec une capacité de chute lente. Faites un double saut pour activer: " .. SpellNames.spells .. "\nNe fonctionne pas en combat ou à cheval."
+	L["Oh no!"] = "Salut l'ami! Ce message est là pour vous informer que l'addon SlowFaller est activé sur une classe sans capacité de chute lente, et n'exécutera donc aucun code.\nRequiert: "  .. SpellNames.spells
+	L["Name"] = "SlowFaller"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "ptBR" then
+	-- Brazilian Portuguese translations go here
+	L["Hello!"] = "Olá amiga! O complemento SlowFaller agora está habilitado em uma classe com uma habilidade de queda lenta. Dê um salto duplo para ativar: " .. SpellNames.spells .. "\nNão funciona em combate ou montado."
+	L["Oh no!"] = "Olá amiga! Esta mensagem está aqui para informar que o addon SlowFaller está ativado em uma classe sem uma capacidade de queda lenta e, portanto, não executará nenhum código.\nRequer: "  .. SpellNames.spells
+	L["Name"] = "SlowFaller"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+	-- Note that the EU Portuguese WoW client also
+	-- uses the Brazilian Portuguese locale code.
+return end
+
+if LOCALE == "ruRU" then
+	-- Russian translations go here
+	L["Hello!"] = "Привет друг! Надстройка двойной-прыжок-замедленное-падение теперь включена для класса со способностью медленного падения. Двойной прыжок для активации: " .. SpellNames.spells .. "\nНе работает в бою или верхом."
+	L["Oh no!"] = "Привет друг! Это сообщение здесь, чтобы сообщить вам, что надстройка двойной-прыжок-замедленное-падение включена для класса без возможности медленного падения, и поэтому не будет запускать какой-либо код.\nТребуется: "  .. SpellNames.spells
+	L["Name"] = "двойной прыжок замедленное падение"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "koKR" then
+	-- Korean translations go here
+	L["Hello!"] = "안녕 친구! 이중-도약저속-낙하 애드온은 이제 느린 낙하 능력이 있는 클래스에서 활성화됩니다. 두 번 점프하여 활성화: " .. SpellNames.spells .. "\n전투 중이나 탈것에 탄 상태에서는 작동하지 않습니다."
+	L["Oh no!"] = "안녕 친구! 이 메시지는 느린 낙하 기능이 없는 클래스에서 이중-도약저속-낙하 애드온이 활성화되어 있으므로 어떤 코드도 실행하지 않을 것임을 알려드리기 위해 여기에 있습니다.\n필요: " .. SpellNames.spells
+	L["Name"] = "이중 도약 저속 낙하"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "zhCN" then
+	-- Simplified Chinese translations go here
+	L["Hello!"] = "你好朋友！ 二段跳缓落术 插件现在在具有缓慢下降能力的类上启用。 双跳激活： " .. SpellNames.spells .. "\n在战斗或骑乘时不起作用。"
+	L["Oh no!"] = "你好朋友！ 这条消息是为了让您知道 二段跳缓落术 插件在没有缓慢下降能力的类上启用，因此不会运行任何代码。\n需要: " .. SpellNames.spells
+	L["Name"] = "二段跳缓落术"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
+
+if LOCALE == "zhTW" then
+	-- Traditional Chinese translations go here
+	L["Hello!"] = "你好朋友！ 二段跳缓落术 插件現在在具有緩慢下降能力的類上啟用。 双跳激活： " .. SpellNames.spells .. "\n在戰鬥或騎乘時不起作用。"
+	L["Oh no!"] = "你好朋友！ 這條消息是為了讓您知道 二段跳缓落术 插件在沒有緩慢下降能力的類上啟用，因此不會運行任何代碼。\n需要: " .. SpellNames.spells
+	L["Name"] = "二段跳缓落术"
+	L["Flight Form"] = Flightform
+	L["Flap"] = Flap
+	L["Slow Fall"] = Slowfall
+	L["Levitate"] = Levitate
+return end
---
+++ SlowFaller.lua
@@ -0,0 +1,219 @@
+local _, L = ...
+
+local defaultsTable = {
+	Levitate = true,
+	Slowfall = true,
+	Flap = true,
+	Flightform = true,
+	Zenflight = true,
+	Gliders = false,
+	Custom = false,
+};
+
+local SlowFallerPanel = CreateFrame("Frame")
+SlowFallerPanel:RegisterEvent("ADDON_LOADED")
+
+function SlowFallerPanel:InitializeOptions(event, arg1)
+	if event == "ADDON_LOADED" and arg1 == "SlowFaller" then
+		if not SlowFaller_DB then
+			SlowFaller_DB = defaultsTable
+		end
+
+		print("initializing SlowFaller addon")
+		SlowFallerPanel.name = L["Name"]
+		InterfaceOptions_AddCategory(SlowFallerPanel)
+
+		-- Create the scrolling parent frame and size it to fit inside the texture
+		SlowFallerPanel.scrollFrame = CreateFrame("ScrollFrame", nil, SlowFallerPanel, "UIPanelScrollFrameTemplate")
+		SlowFallerPanel.scrollFrame:SetPoint("TOPLEFT", 3, -4)
+		SlowFallerPanel.scrollFrame:SetPoint("BOTTOMRIGHT", -27, 4)
+
+		-- Create the scrolling child frame, set its width to fit, and give it an arbitrary minimum height (such as 1)
+		SlowFallerPanel.scrollChild = CreateFrame("Frame")
+		SlowFallerPanel.scrollFrame:SetScrollChild(SlowFallerPanel.scrollChild)
+		SlowFallerPanel.scrollChild:SetWidth(SettingsPanel.Container:GetWidth()-18)
+		SlowFallerPanel.scrollChild:SetHeight(1) 
+
+		-- Add widgets to the scrolling child frame as desired
+		SlowFallerPanel.title = SlowFallerPanel.scrollChild:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+		SlowFallerPanel.title:SetPoint("TOPLEFT", 10, -15)
+		SlowFallerPanel.title:SetText(L["Name"])
+
+		SlowFallerPanel.CheckBox_Priest = CreateFrame("CheckButton", "SF_PriestLeviOptionsCheckbox", SlowFallerPanel.scrollChild, "UICheckButtonTemplate");
+		SlowFallerPanel.CheckBox_Priest:ClearAllPoints();
+		SlowFallerPanel.CheckBox_Priest:SetPoint("TOPLEFT", 50, -53);
+		getglobal(SlowFallerPanel.CheckBox_Priest:GetName().."Text"):SetText(L["Levitate"]);
+
+		SlowFallerPanel.CheckBox_Priest:SetScript("OnClick", function(self)
+			if SlowFallerPanel.CheckBox_Priest:GetChecked() then
+				SlowFaller_DB.Levitate = true;
+			else
+				SlowFaller_DB.Levitate = false;
+			end
+		end);
+
+		SlowFallerPanel.CheckBox_Mage = CreateFrame("CheckButton", "SF_MageSlowfallOptionsCheckbox", SlowFallerPanel.scrollChild, "UICheckButtonTemplate");
+		SlowFallerPanel.CheckBox_Mage:ClearAllPoints();
+		SlowFallerPanel.CheckBox_Mage:SetPoint("TOPLEFT", 50, -53*1.5);
+		getglobal(SlowFallerPanel.CheckBox_Mage:GetName().."Text"):SetText(L["Slow Fall"]);
+
+		SlowFallerPanel.CheckBox_Mage:SetScript("OnClick", function(self)
+			if SlowFallerPanel.CheckBox_Mage:GetChecked() then
+				SlowFaller_DB.Slowfall = true;
+			else
+				SlowFaller_DB.Slowfall = false;
+			end
+		end);
+
+		SlowFallerPanel.CheckBox_Druid = CreateFrame("CheckButton", "SF_DruidFlapOptionsCheckbox", SlowFallerPanel.scrollChild, "UICheckButtonTemplate");
+		SlowFallerPanel.CheckBox_Druid:ClearAllPoints();
+		SlowFallerPanel.CheckBox_Druid:SetPoint("TOPLEFT", 50, -53*2);
+		getglobal(SlowFallerPanel.CheckBox_Druid:GetName().."Text"):SetText(L["Flap"]);
+
+		SlowFallerPanel.CheckBox_Druid:SetScript("OnClick", function(self)
+			if SlowFallerPanel.CheckBox_Druid:GetChecked() then
+				SlowFaller_DB.Flap = true;
+			else
+				SlowFaller_DB.Flap = false;
+			end
+		end);
+
+		SlowFallerPanel.CheckBox_Priest:SetChecked(SlowFaller_DB.Levitate)
+		SlowFallerPanel.CheckBox_Mage:SetChecked(SlowFaller_DB.Slowfall)
+		SlowFallerPanel.CheckBox_Druid:SetChecked(SlowFaller_DB.Flap)
+
+		--[[
+		SlowFallerPanel.footer = SlowFallerPanel.scrollChild:CreateFontString("ARTWORK", nil, "GameFontNormal")
+		SlowFallerPanel.footer:SetPoint("TOP", 0, -5000)
+		SlowFallerPanel.footer:SetText("This is 5000 below the top, so the scrollChild automatically expanded.")
+		]]
+
+	end
+end
+
+
+SlowFallerPanel:SetScript("OnEvent", SlowFallerPanel.InitializeOptions)
+
+
+local f = CreateFrame("Frame");
+f:RegisterEvent("PLAYER_ENTERING_WORLD");
+f.havewemet = false
+
+f.spellID = 130
+f.buffID = 130
+f.priest = select(2, UnitClassBase("player")) == 5
+f.mage = select(2, UnitClassBase("player")) == 8
+f.monk = select(2, UnitClassBase("player")) == 10
+f.druid = select(2, UnitClassBase("player")) == 11
+
+if f.priest then -- priest
+	f.spellID = 1706 -- levitate spell
+	f.buffID = 111759 -- levitate buff
+elseif f.mage then -- mage
+	f.spellID = 130 -- slow fall spell
+	f.buffID = 130 -- slow fall buff
+--[[
+elseif f.monk then -- monk (needs more work for the cancel action, will look at it later)
+	f.spellID = 125883 -- zen flight spell
+	f.buffID = 125883 -- zen flight buff
+	]]
+elseif f.druid then -- druid
+	f.spellID = 164862 -- flap spell
+	f.buffID = 164862 -- flap buff
+else
+	f:SetScript("OnEvent", function()
+		if f.havewemet == false then
+			DEFAULT_CHAT_FRAME:AddMessage(L["Oh no!"])
+			f.havewemet = true
+		end
+		--Hello friend! The SlowFaller addon is enabled but will not run any code because this class does not have slow fall, flap, or levitate.
+	end)
+	return
+end
+f:SetScript("OnEvent", function()
+	if f.havewemet == false then
+		DEFAULT_CHAT_FRAME:AddMessage(L["Hello!"])
+		f.havewemet = true
+	end
+	--Hello friend! The SlowFaller addon is now enabled on a class with a slow fall ability. Double-jump to activate slow fall, flap, or levitate. Does not work in combat or while mounted.
+end)
+
+local INVALID_SPELL = {
+	302677, -- Anti-Gravity Pack
+	303841, -- Anti-Gravity Pack
+	313053, -- Faerie's Blessing
+	323695, -- Faerie Dust
+	356280, -- Dragon Companion's Vigilance
+	383363, -- Lift Off
+	--1459, -- Arcane Intellect test
+};
+
+local SlowFallEvent = CreateFrame("Frame")
+
+--SlowFallEvent:RegisterEvent("ADDON_LOADED");
+SlowFallEvent:RegisterEvent("PLAYER_ENTERING_WORLD");
+
+function SlowFallEvent:OnEvent(event,arg1)
+
+	if event ~= "PLAYER_ENTERING_WORLD" then
+		ClearOverrideBindings(SlowFallEvent);
+	end
+
+	if event == "PLAYER_ENTERING_WORLD" then
+		SlowFallEvent:RegisterEvent("PLAYER_LOGOUT");
+		SlowFallEvent:RegisterEvent("PLAYER_REGEN_DISABLED");
+		SlowFallEvent:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED");
+
+		local spell = Spell:CreateFromSpellID(f.spellID);
+		local buff = Spell:CreateFromSpellID(f.buffID);
+		local key1, key2 = GetBindingKey("JUMP");
+		--local badBuff = C_UnitAuras.GetPlayerAuraBySpellID(INVALID_SPELL[f.spellID]);
+
+		function SlowFallEvent.Jumpy(self, key)
+			--print(key) -- debug to find what key is pressed
+			if UnitAffectingCombat("player") then
+				return
+			elseif IsMounted() or select(1, IsUsableSpell(f.spellID)) == false  then --or (not badBuff) or (not C_UnitAuras.GetPlayerAuraBySpellID(INVALID_SPELL[k]))
+				ClearOverrideBindings(SlowFallEvent);
+				return
+			else
+				key1, key2 = GetBindingKey("JUMP");
+				local aura = C_UnitAuras.GetPlayerAuraBySpellID(f.spellID) or C_UnitAuras.GetPlayerAuraBySpellID(f.buffID);
+				if key == ( key1 or key2 ) then
+					for k,v in pairs(INVALID_SPELL) do
+						if C_UnitAuras.GetPlayerAuraBySpellID(INVALID_SPELL[k]) then
+							return
+						end
+					end
+					if IsFalling() == true then
+
+						if f.priest and SlowFaller_DB.Levitate == false then
+							return
+						end
+						if f.mage and SlowFaller_DB.Slowfall == false then
+							return
+						end
+						if f.druid and SlowFaller_DB.Flap == false then
+							return
+						end
+
+						if aura then
+							CancelSpellByName(spell:GetSpellName());
+							ClearOverrideBindings(SlowFallEvent);
+							return
+						else
+							SetOverrideBinding(SlowFallEvent, true, key1, "SPELL " .. spell:GetSpellName());
+						end
+					end
+					if IsFalling() == false then
+						ClearOverrideBindings(SlowFallEvent);
+					end
+				end
+			end
+		end
+
+		SlowFallEvent:SetScript("OnKeyDown", SlowFallEvent.Jumpy);
+		SlowFallEvent:SetPropagateKeyboardInput(true);
+	end
+end
+SlowFallEvent:SetScript("OnEvent",SlowFallEvent.OnEvent);
